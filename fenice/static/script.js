da9288aa1fe37ab1ab9510de36ff0d75def2567b
vitoraugustto <60718553+vitoraugustto@users.noreply.github.com>
2021-03-01 20:52:07 -0300

    Add files via upload

---
+++ fenice/static/script.js
@@ -0,0 +1,14 @@
+window.onload = function() {
+
+const input = document.getElementById('menuInput');
+const lupa = document.getElementById('lupa');
+
+
+  lupa.addEventListener('mouseenter', function() {
+    input.style.width = '179px'
+    lupa.style.right = '179px'
+
+
+  });
+
+};
